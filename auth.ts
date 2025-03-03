6ab1b28474cd52947d135148a276a83ed78436f3
Niklas Magnusson <niklas.magnusson@picadeli.se>
2025-03-03 20:47:14 +0100

    add oauth stuff

--- auth.ts
+++ auth.ts
@@ -1,6 +1,26 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
-export const { handlers, auth } = NextAuth({
+export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [GitHub],
+    // callbacks: {
+    //     async signIn({ user, account, profile }) {
+    //         if (account?.provider === "github") {
+    //             const organization = process.env.GITHUB_ORG!;
+    //             const username = profile?.login;
+    //             // GitHub returns 204 for a valid org membership
+    //             const res = await fetch(
+    //                 `https://api.github.com/orgs/${organization}/members/${username}`,
+    //                 {
+    //                     headers: {
+    //                         Accept: "application/vnd.github.v3+json",
+    //                         Authorization: `token ${account.access_token!}`,
+    //                     },
+    //                 },
+    //             );
+    //             return res.status === 204;
+    //         }
+    //         return true;
+    //     },
+    // },
 });
