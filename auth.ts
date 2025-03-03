4512b247cb4c7947de6af56422ad3760bfb5eacd
Niklas Magnusson <niklas.magnusson@picadeli.se>
2025-03-03 21:05:33 +0100

    add .nvmrc

--- auth.ts
+++ auth.ts
@@ -3,24 +3,26 @@ import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [GitHub],
-    // callbacks: {
-    //     async signIn({ user, account, profile }) {
-    //         if (account?.provider === "github") {
-    //             const organization = process.env.GITHUB_ORG!;
-    //             const username = profile?.login;
-    //             // GitHub returns 204 for a valid org membership
-    //             const res = await fetch(
-    //                 `https://api.github.com/orgs/${organization}/members/${username}`,
-    //                 {
-    //                     headers: {
-    //                         Accept: "application/vnd.github.v3+json",
-    //                         Authorization: `token ${account.access_token!}`,
-    //                     },
-    //                 },
-    //             );
-    //             return res.status === 204;
-    //         }
-    //         return true;
-    //     },
-    // },
+    callbacks: {
+        async signIn({ user, account, profile }) {
+            if (account?.provider === "github") {
+                console.log("signIn", { user, account, profile });
+                const organization = process.env.GITHUB_ORG!;
+                const username = profile?.login;
+                // GitHub returns 204 for a valid org membership
+                const res = await fetch(
+                    `https://api.github.com/orgs/${organization}/members/${username}`,
+                    {
+                        headers: {
+                            Accept: "application/vnd.github.v3+json",
+                            Authorization: `token ${account.access_token!}`,
+                        },
+                    },
+                );
+                console.log("signIn res", res);
+                return res.status === 204;
+            }
+            return true;
+        },
+    },
 });
