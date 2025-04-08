dcc7a672bcb1486f52a018570011566f735244a5
nmagnusson <niklas.j.magnusson@gmail.com>
2025-04-08 20:56:37 +0200

    Restrict access by default unless logged in (#54)
    Middleware handles exceptions: black list and white list

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,6 @@
 export { auth as middleware } from "@/auth";
+
+// Add a matcher configuration to exclude paths that shouldn't be protected
+export const config = {
+    matcher: ["/((?!auth/error|api|_next/static|_next/image|favicon.ico).*)"],
+};
