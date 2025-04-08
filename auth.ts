8b85c090cd3ccc2a4a1492b053700f0015c893f3
Niklas Magnusson <niklas.j.magnusson@gmail.com>
2025-04-08 17:25:03 +0200

    Restrict pages by default unless logged in
    Middleware handles exceptions: black list and white list

--- auth.ts
+++ auth.ts
@@ -4,6 +4,10 @@ import GitHub from "next-auth/providers/github";
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [GitHub],
     callbacks: {
+        authorized: async ({ auth }) => {
+            // Logged in users are authenticated, otherwise redirect to login page
+            return !!auth?.user;
+        },
         async signIn({ account, profile }) {
             if (account?.provider === "github") {
                 // Verify that the user is a public member of the organization
--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,14 @@
 export { auth as middleware } from "@/auth";
+
+// Define which routes require authentication and which don't
+export const config = {
+    matcher: [
+        // Protected routes that require authentication
+        "/dashboard/:path*",
+        "/profile/:path*",
+        "/api/:path*",
+
+        // Don't protect these routes
+        "/((?!api|_next/static|_next/image|favicon.ico|auth|$).*)",
+    ],
+};
