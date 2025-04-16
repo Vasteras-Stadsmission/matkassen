ed306950553d47afdc62a63d2140c495ed8f5358
Niklas Magnusson <niklas.j.magnusson@gmail.com>
2025-04-16 22:22:08 +0200

    bunch of stuff

--- app/db/actions.ts
+++ app/db/actions.ts
@@ -2,17 +2,28 @@
 
 import { revalidatePath } from "next/cache";
 import { db } from "./drizzle";
-import { todos } from "./schema";
+import { households } from "./schema";
 import { eq } from "drizzle-orm";
 
-export async function addTodoAction(formData: FormData) {
-    const content = formData.get("content") as string;
-    await db.insert(todos).values({ content });
+export async function addHouseholdAction(formData: FormData) {
+    const firstName = formData.get("first_name") as string;
+    const lastName = formData.get("last_name") as string;
+    const phoneNumber = formData.get("phone_number") as string;
+    const locale = formData.get("locale") as string;
+    const postalCode = Number(formData.get("postal_code"));
+
+    await db.insert(households).values({
+        first_name: firstName,
+        last_name: lastName,
+        phone_number: phoneNumber,
+        locale: locale,
+        postal_code: postalCode
+    });
     revalidatePath("/db");
 }
 
-export async function deleteTodoAction(formData: FormData) {
+export async function deleteHouseholdAction(formData: FormData) {
     const id = formData.get("id") as string;
-    await db.delete(todos).where(eq(todos.id, Number(id)));
+    await db.delete(households).where(eq(households.id, id));
     revalidatePath("/db");
 }
--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,8 +1,6 @@
 import { sql } from "drizzle-orm";
 import {
     pgTable,
-    uuid,
-    serial,
     timestamp,
     varchar,
     integer,
@@ -10,7 +8,15 @@ import {
     boolean,
     pgEnum,
     check,
+    foreignKey,
 } from "drizzle-orm/pg-core";
+import { customAlphabet } from "nanoid";
+
+// Needed to create a default nanoid value for the primary key
+export const nanoid = (length = 14) => {
+    const alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    return customAlphabet(alphabet, length)();
+};
 
 export const sexEnum = pgEnum("sex", ["male", "female", "other"]);
 export const petSpeciesEnum = pgEnum("pet_species", ["dog", "cat", "bunny", "bird"]);
@@ -18,7 +24,10 @@ export const petSpeciesEnum = pgEnum("pet_species", ["dog", "cat", "bunny", "bir
 export const households = pgTable(
     "households",
     {
-        id: uuid("id").primaryKey(),
+        id: text("id")
+            .primaryKey()
+            .notNull()
+            .$defaultFn(() => nanoid(6)),
         created_at: timestamp({ precision: 1, withTimezone: true }).defaultNow().notNull(), // will determine end of lifecycle
         first_name: varchar("first_name", { length: 50 }).notNull(),
         last_name: varchar("last_name", { length: 50 }).notNull(),
@@ -30,8 +39,11 @@ export const households = pgTable(
 );
 
 export const householdComments = pgTable("household_comments", {
-    id: integer("id").primaryKey(),
-    household_id: uuid("household_id")
+    id: text("id")
+        .primaryKey()
+        .notNull()
+        .$defaultFn(() => nanoid(6)),
+    household_id: text("household_id")
         .notNull()
         .references(() => households.id),
     created_at: timestamp({ precision: 1, withTimezone: true }).defaultNow().notNull(),
@@ -39,9 +51,12 @@ export const householdComments = pgTable("household_comments", {
 });
 
 export const householdMembers = pgTable("household_members", {
-    id: integer("id").primaryKey(),
+    id: text("id")
+        .primaryKey()
+        .notNull()
+        .$defaultFn(() => nanoid(6)),
     created_at: timestamp({ precision: 1, withTimezone: true }).defaultNow().notNull(),
-    household_id: uuid("household_id")
+    household_id: text("household_id")
         .notNull()
         .references(() => households.id),
     age: integer("age").notNull(),
@@ -49,32 +64,44 @@ export const householdMembers = pgTable("household_members", {
 });
 
 export const pets = pgTable("pets", {
-    id: integer("id").primaryKey(),
+    id: text("id")
+        .primaryKey()
+        .notNull()
+        .$defaultFn(() => nanoid(6)),
     created_at: timestamp({ precision: 1, withTimezone: true }).defaultNow().notNull(),
-    household_id: uuid("household_id")
+    household_id: text("household_id")
         .notNull()
         .references(() => households.id),
     species: petSpeciesEnum("species").notNull(),
 });
 
 export const foodParcels = pgTable("food_parcels", {
-    id: uuid("id").primaryKey(),
-    household_id: uuid("household_id")
+    id: text("id")
+        .primaryKey()
+        .notNull()
+        .$defaultFn(() => nanoid(6)),
+    household_id: text("household_id")
         .notNull()
         .references(() => households.id),
-    pickup_location: integer("pickup_location")
+    pickup_location: text("pickup_location")
         .notNull()
         .references(() => pickupLocations.id),
     pickup_date_time_earliest: timestamp({ precision: 0, withTimezone: true }).notNull(),
     pickup_date_time_latest: timestamp({ precision: 0, withTimezone: true }).notNull(),
     is_picked_up: boolean("is_picked_up").notNull().default(false),
-    url_uid: text("url_uid").notNull().unique(), // For shared urls, or make the id a nanoid directly?
+    url_uid: text("url_uid")
+        .notNull()
+        .unique()
+        .$defaultFn(() => nanoid(6)), // For shared urls
 });
 
 export const pickupLocations = pgTable(
     "pickup_locations",
     {
-        id: serial("id").primaryKey(), // auto-incremented integer
+        id: text("id")
+            .primaryKey()
+            .notNull()
+            .$defaultFn(() => nanoid(6)),
         name: text("name").notNull(), // e.g., Västerås Stadsmission, Klara Kyrka, Frihamnskyrkan...
         street_address: text("street_address").notNull(),
         postal_code: integer("postal_code").notNull(),
@@ -94,32 +121,76 @@ export const pickupLocations = pgTable(
     },
 );
 
-export const householdDietaryRestrictions = pgTable("household_dietary_restrictions", {
-    id: serial("id").primaryKey(),
-    household_id: uuid("household_id")
-        .notNull()
-        .references(() => households.id),
-    dietary_restriction_id: integer("dietary_restriction_id")
-        .notNull()
-        .references(() => dietaryRestrictions.id),
-});
+export const householdDietaryRestrictions = pgTable(
+    "household_dietary_restrictions",
+    {
+        id: text("id")
+            .primaryKey()
+            .notNull()
+            .$defaultFn(() => nanoid(6)),
+        household_id: text("household_id")
+            .notNull()
+            .references(() => households.id),
+        dietary_restriction_id: text("dietary_restriction_id")
+            .notNull()
+            .references(() => dietaryRestrictions.id),
+    },
+    table => [
+        foreignKey({
+            name: "household_dietary_restrictions_household_id_fk",
+            columns: [table.household_id],
+            foreignColumns: [households.id],
+        }).onDelete("cascade"),
+
+        foreignKey({
+            name: "household_dietary_restrictions_restriction_id_fk",
+            columns: [table.dietary_restriction_id],
+            foreignColumns: [dietaryRestrictions.id],
+        }).onDelete("restrict"),
+    ],
+);
 
 export const dietaryRestrictions = pgTable("dietary_restrictions", {
-    id: serial("id").primaryKey(),
+    id: text("id")
+        .primaryKey()
+        .notNull()
+        .$defaultFn(() => nanoid(6)),
     name: text("name").notNull(), // e.g., gluten, lactose, pork...
 });
 
-export const householdAdditionalNeeds = pgTable("household_additional_needs", {
-    id: serial("id").primaryKey(),
-    household_id: uuid("household_id")
-        .notNull()
-        .references(() => households.id),
-    additional_need_id: integer("additional_need_id")
-        .notNull()
-        .references(() => additionalNeeds.id), // TODO: Change to https://orm.drizzle.team/docs/relations#one-to-many etc
-});
+export const householdAdditionalNeeds = pgTable(
+    "household_additional_needs",
+    {
+        id: text("id")
+            .primaryKey()
+            .notNull()
+            .$defaultFn(() => nanoid(6)),
+        household_id: text("household_id")
+            .notNull()
+            .references(() => households.id),
+        additional_need_id: text("additional_need_id")
+            .notNull()
+            .references(() => additionalNeeds.id),
+    },
+    table => [
+        foreignKey({
+            name: "household_additional_needs_household_id_fk",
+            columns: [table.household_id],
+            foreignColumns: [households.id],
+        }).onDelete("cascade"),
+
+        foreignKey({
+            name: "household_additional_needs_need_id_fk",
+            columns: [table.additional_need_id],
+            foreignColumns: [additionalNeeds.id],
+        }).onDelete("restrict"),
+    ],
+);
 
 export const additionalNeeds = pgTable("additional_needs", {
-    id: serial("id").primaryKey(),
+    id: text("id")
+        .primaryKey()
+        .notNull()
+        .$defaultFn(() => nanoid(6)),
     need: text("need").notNull(), // e.g., diapers, bus pass, cleaning supplies...
 });
