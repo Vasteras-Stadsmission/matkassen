0ec44c3f570bea92d84ad6b2040bdea4e5b71567
Niklas Magnusson <niklas.magnusson@picadeli.se>
2025-06-02 08:02:09 +0200

    Add more auth protection and copilot instructions

--- app/[locale]/handout-locations/page.tsx
+++ app/[locale]/handout-locations/page.tsx
@@ -3,6 +3,7 @@ import { Skeleton } from "@mantine/core";
 import { HandoutLocationsContent } from "./components/HandoutLocationsContent";
 import { HandoutLocationsPageLayout } from "./components/HandoutLocationsPageLayout";
 import { getLocations } from "./actions";
+import { AuthProtection } from "@/components/AuthProtection";
 
 // This needs to be dynamic to avoid build time issues in CI
 export const dynamic = "force-dynamic";
@@ -12,19 +13,21 @@ export default async function HandoutLocationsPage() {
     const locations = await getLocations();
 
     return (
-        <HandoutLocationsPageLayout>
-            <Suspense
-                fallback={
-                    <>
-                        <Skeleton height={50} mb="md" width="50%" />
-                        <Skeleton height={200} mb="md" />
-                        <Skeleton height={200} mb="md" />
-                    </>
-                }
-            >
-                {/* Pass the data down as props */}
-                <HandoutLocationsContent initialLocations={locations} />
-            </Suspense>
-        </HandoutLocationsPageLayout>
+        <AuthProtection>
+            <HandoutLocationsPageLayout>
+                <Suspense
+                    fallback={
+                        <>
+                            <Skeleton height={50} mb="md" width="50%" />
+                            <Skeleton height={200} mb="md" />
+                            <Skeleton height={200} mb="md" />
+                        </>
+                    }
+                >
+                    {/* Pass the data down as props */}
+                    <HandoutLocationsContent initialLocations={locations} />
+                </Suspense>
+            </HandoutLocationsPageLayout>
+        </AuthProtection>
     );
 }
