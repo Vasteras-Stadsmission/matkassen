444108d878983ba5dbe9a791031b6edc5b6ac533
Niklas Magnusson <niklas.j.magnusson@gmail.com>
2026-06-20 23:45:41 +0200

    Remove legacy handout locations route

--- app/[locale]/handout-locations/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getLocale } from "next-intl/server";
-import { redirect } from "@/app/i18n/navigation";
-
-export default async function HandoutLocationsPage() {
-    const locale = await getLocale();
-    redirect({ href: "/settings/locations", locale });
-}
